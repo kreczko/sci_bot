46a69e49817276585b74be6b9c11620beb4344b5
Lukasz Kreczko <lkreczko@googlemail.com>
2018-01-04 09:37:58 +0100

    added msg identifiers

--- sci_bot/gitlab.py
+++ sci_bot/gitlab.py
@@ -28,3 +28,24 @@ def add_label_to_merge_request(label, merge_request):
 def remove_label_from_merge_request(label, merge_request):
     merge_request.labels.remove(label)
     merge_request.save()
+
+def __is_object_kind__(msg, kind):
+    return 'object_kind' in msg and msg['object_kind'] == kind
+
+def is_push_event(msg):
+    return __is_object_kind__(msg, 'push')
+
+def is_tag_event(msg):
+    return __is_object_kind__(msg, 'tag_push')
+
+def is_issue_event(msg):
+    return __is_object_kind__(msg, 'issue')
+
+def is_merge_request_event(msg):
+    return __is_object_kind__(msg, 'merge_request')
+
+def is_ci_event(msg):
+    return __is_object_kind__(msg, 'pipeline')
+
+def is_build_event(msg):
+    return __is_object_kind__(msg, 'build')
