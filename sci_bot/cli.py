1898073c0d9d227a62982f0adcee2bca4b4c6e3d
Lukasz Kreczko <lkreczko@googlemail.com>
2018-01-04 09:38:24 +0100

    added improvements to cli

--- sci_bot/cli.py
+++ sci_bot/cli.py
@@ -1,52 +1,34 @@
 # -*- coding: utf-8 -*-
 
 """Console script for sci_bot."""
-
 import click
 import yaml
-import gitlab
 import os
-# import requests_cache
 import pickle
 
-# requests_cache.install_cache('api_cache')
-
 from . import gitlab as gl
-
-
-def get_projects(conn, cachefile='cache.pkl'):
-    if not os.path.exists(cachefile):
-        projects = conn.projects.list(all=True)
-        projects_to_cachefile(projects, cachefile)
-        return projects
-    return projects_from_cachefile(cachefile)
-
-
-def projects_from_cachefile(cachefile):
-    with open(cachefile, 'rb') as f:
-        projects = pickle.load(f)
-    return projects
-
-
-def projects_to_cachefile(projects, cachefile):
-    with open(cachefile, 'wb') as f:
-        pickle.dump(projects, f)
-
+from .logger import log
+from dotenv import load_dotenv
+from .ci_bot import listen_kafka
 
 @click.command()
 @click.option('-c', '--config', type=click.File(), default='private_config.yaml')
 @click.option('-i', '--ignore-cache', default=False)
-def main(config, ignore_cache, args=None):
+@click.option('-e', '--env_file', type=click.Path(exists=True), default='.env')
+def main(config, ignore_cache, env_file, args=None):
     """Console script for sci_bot."""
-    config = yaml.load(config)
-    print(config)
+    # TODO: Config should come from a git repo, instructions similar to
+    # gitlab runner's config.yoml
+    load_dotenv(env_file)
+    config = read_config(config)
+
     connection = gl.connect(config['repo'], config['API_TOKEN'])
 
     remote_projects = gl.get_list_of_projects(connection)
     projects = config['projects']
     found_projects = []
 
-    print(len(remote_projects))
+    log.debug('Found %d remote projects', len(remote_projects))
     for p in remote_projects:
         # print(p.path_with_namespace)
         for project in projects:
@@ -55,10 +37,21 @@ def main(config, ignore_cache, args=None):
             found_projects.append(p)
 
     for project in found_projects:
-        print('Found', project.path_with_namespace)
+        log.debug('Found %s', project.path_with_namespace)
         mrs = gl.get_merge_requests(project)
-        print('N MRs:', len(mrs))
+        log.debug('N MRs: %d', len(mrs))
 
+    listen_kafka(config)
+
+
+def read_config(config_file):
+    config = yaml.load(config_file)
+    log.debug(config)
+    # TODO:some checks
+    return config
+
+def listen(topics):
+    pass
 
 if __name__ == "__main__":
     main()
